67f239ac088e5be7f7654da21190e0546140fb4e
Ermin Cahtarevic <erminc@windowslive.com>
2019-12-24 20:38:37 +0100

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #handle
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   private
-
+  
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
